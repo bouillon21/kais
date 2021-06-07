9a738aa9ecc88ad51b75ca06e43e7048e2d31428
Bulat <b.barzhanov@yandex.ru>
2021-06-08 00:40:24 +0300

    add wall and anim

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 moveVector;
+    public Animator anim;
+    public SpriteRenderer sr;
+    public float speed = 2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Walk();
+        Flip();
+    }
+
+    void Walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        sr.flipX = moveVector.x < 0;
+    }
+}
