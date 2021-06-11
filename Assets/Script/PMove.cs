be689e94b90f521ac0408940a4b64d35e31a19bd
Bulat <b.barzhanov@yandex.ru>
2021-06-11 16:12:19 +0300

    Исправил баг в повороте персонажа

--- Assets/Script/PMove.cs
+++ Assets/Script/PMove.cs
@@ -9,6 +9,8 @@ public class PMove : MonoBehaviour
     public Animator anim;
     public SpriteRenderer sr;
     public float speed = 2;
+    //public int directionInput;
+    public bool faceRigh = true;
 
     void Start()
     {
@@ -32,6 +34,12 @@ public class PMove : MonoBehaviour
 
     void Flip()
     {
-        sr.flipX = moveVector.x < 0;
+        if ((moveVector.x > 0 && !faceRigh) || (moveVector.x < 0 && faceRigh))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+            faceRigh = !faceRigh;
+        }
+
+        //sr.flipX = moveVector.x < 0;
     }
 }
